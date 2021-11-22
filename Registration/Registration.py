886b4c3bf444069cd21fbcb97c4853ea1a5bb98c
Maev Alexey <alexborn950@gmail.com>
2021-11-22 21:57:21 +0200

    Done

--- Registration/Registration.py
+++ Registration/Registration.py
@@ -1,6 +1,7 @@
 import random
-login_list = []
-pass_list = []
+import re
+login_list = ["Alex"]
+pass_list = ["Aa1!aaaa"]
 
 def username(n: str, l: list):
     """Sisestatud kasutaja nimi otsing
@@ -23,21 +24,57 @@ def auto_reg():
     :rtype: str
     """
     str0 = ".,:;!_*-+()/#¤%&"
-    str1 = '0123456789'
-    str2 = 'qwertyuiopasdfghjklzxcvbnm'
+    str1 = "0123456789"
+    str2 = "qwertyuiopasdfghjklzxcvbnm"
     str3 = str2.upper()
     str4 = str0+str1+str2+str3
     ls = list(str4)
     random.shuffle(ls)
     # Извлекаем из списка 12 произвольных значений
-    psword = ''.join([random.choice(ls) for x in range(12)])
+    password = ''.join([random.choice(ls) for x in range(12)])
     # Пароль готов
-    print(psword)
-    return psword
+    print(f"Ваш пароль: {password}")
+    return password
 def my_reg():
-    pass
-def author():
-    return
+    '''
+    Salasõna kirjutamine
+    
+    Tagastab salasõna str formaadis
+    
+    :rtype: str
+    '''
+    print("Пароль должен содержать: минимум 8 символов, 1 букву в верхнем регистре, 1 букву в нижнем регистре, 1 спецсимол, 1 цифру")
+    password = input("Введите пароль: ")
+    sym = ".,:;!_*-+()/#¤%&)"
+    i = 0
+    while True:
+        if passcheck(password) == True:
+            break
+        else:
+            password = input("Введите пароль: ")
+           
+    return password
+def passcheck(p: str):
+    '''Parooli kontroll
+
+    Tagastab True/False
+
+    :param str p: Parool
+    :rtype: bool
+    '''
+    if len(p) < 8:
+        print("Пароль должен содержать минимум 8 символов")
+    elif re.search('[A-Z]', p) is None:
+        print("Пароль должен содержать буквы в верхнем регистре")
+    elif re.search('[a-z]', p) is None:
+        print("Пароль должен содержать буквы в нижнем регистре")
+    elif re.search('[.,:;!_*-+()/\#¤%&@]', p) is None:
+        print("Пароль должен содержать спецсимволы")
+    elif re.search('[0-9]', p) is None:
+        print("Пароль должен содержать цифры")
+    else:
+        return True
+    return False
 def reg(l: list, p: list):   
     """Inimese registreerimine
     
@@ -47,36 +84,65 @@ def reg(l: list, p: list):
     :rtype: list,list
     """
     print("Регистрация")
-    n = input("Введите логин: ")
-    u = username(n, login_list)
+
+    log = input("Введите логин: ")
+    u = username(log, login_list)
+    i = 0
     while u == True:
-        n = input("Такой логин уже существует. Введите  логин еще раз: ")
-        u = username(n, login_list)
-    v = input("Создать пароль автоматически или нет? (y/n): ")
+        print("Данный логин уже существует.")
+        log = input("Введите другой логин: ")
+        u = username(log, login_list)
+
+
+    v = input("Создать пароль автоматически? (y/n): ")
     if v == "y":
         p = auto_reg()
-        login_list.append(n)
-        pass_list.append(p)
     else:
-        my_reg()
+        p = my_reg()
+
+    login_list.append(log)
+    pass_list.append(p)
+    print("Аккаунт создан")
     return l, p
+def author():
+    '''Autoriseerimine
+
+    Tagastab True/False
+    
+    :rtype: bool
+    '''
+    x = input("Введите логин: ")
+    u = username(x, login_list)
+    if u == True:
+        login = login_list.index(x)
+        x = input("Введите пароль: ")
+        if pass_list[login] == x:
+            return True
+        else:
+            print("Неверный пароль")
+            return False
+    else:
+        print("Неверный логин")
+        return False
 
 while 1:
-    print("Регистрация / Авторизиция / Данные / Выход")
-    v = input("1 / 2 / 3 / 4: ")
+    print("Регистрация / Aвторизация / Данные / Выход из системы")
+    v = input("1/ 2/ 3/ 4: ")
     if v == "1":
         reg(login_list, pass_list)
     elif v == "2":
+        print("Авторизация")
         i = 0
         while i < 3:
-            t = author() #True or False
-            if author() == True:
-                print("Добро пожаловать!")
+            t = author()
+            if t == True:
+                print("Авторизация успешна.")
+                i = 3
             else:
                 i+=1
     elif v == "3":
         print(login_list)
         print(pass_list)
     else:
-        print("Выход.")
+        print("The end")
         break
